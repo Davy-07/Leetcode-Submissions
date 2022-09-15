4cceb18799c01e98cdc0e5e63c21c2912576d577
Davy-07 <93039183+Davy-07@users.noreply.github.com>
2022-09-15 19:51:51 +0530

    Time: 15 ms (93.41%), Space: 13.5 MB (89.84%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    class comparator{
+        public:
+        bool operator()(pair<int,int> const &a,pair<int,int> const &b)
+        {
+            return a.second > b.second;
+        }
+            
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        vector<int> ans;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,comparator> pq;
+        for(auto it:m)
+        {
+            pq.push({it.first,it.second});
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+        
+        for(int i=0;i<k;i++)
+        {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+        return ans;
+    }
+};
