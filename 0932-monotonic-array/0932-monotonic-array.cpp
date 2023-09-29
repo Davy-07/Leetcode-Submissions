2abdf9e11fcc1a3691dc4c6a7b3ffde48d434579
Devanshu Mehta <93039183+Davy-07@users.noreply.github.com>
2023-09-30 00:06:50 +0530

    Time: 106 ms (95.55%), Space: 96.9 MB (13.85%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    bool isMonotonic(std::vector<int>& nums) {
-        if (nums.size() < 2) return true;
-
-        int direction = 0;  // 0 means unknown, 1 means increasing, -1 means decreasing
-
-        for (size_t i = 1; i < nums.size(); i++) {
-            if (nums[i] > nums[i-1]) {  // increasing
-                if (direction == 0) direction = 1;
-                else if (direction == -1) return false;
-            } else if (nums[i] < nums[i-1]) {  // decreasing
-                if (direction == 0) direction = -1;
-                else if (direction == 1) return false;
+    bool increasing(std::vector<int>& nums){
+        int ans = true;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1]){
+                ans = false;
+                break;
             }
         }
+        return ans;
+    }
 
-        return true;
+    bool decreasing(std::vector<int>& nums){
+        int ans =true;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<nums[i+1]){
+                ans = false;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    bool isMonotonic(std::vector<int>& nums) {
+        bool inc = increasing(nums);
+        bool dec= decreasing(nums);
+
+        return inc||dec;
     }
 };
